b2372c75602b350c27d4e587f111fb0c1a1483f9
Hardy Lutula <hardylutula@gmail.com>
2023-12-05 09:27:23 +0200

    jjjj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FooterComponent,
-    HeaderComponent,
-    HomeComponent,
-    LandingComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
